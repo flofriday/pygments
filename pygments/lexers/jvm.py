fca6ca58698c4f5e09dd9f9d3f42634c93be1072
flofriday <flohacksfriday@gmail.com>
2025-09-01 09:30:46 +0200

    Temporary solution

--- pygments/lexers/jvm.py
+++ pygments/lexers/jvm.py
@@ -1169,8 +1169,11 @@ class KotlinLexer(RegexLexer):
             (r'"', String, 'string'),
             (r"'\\.'|'[^\\]'", String.Char),
             # Numbers
-            (r"[0-9](\.[0-9]*)?([eE][+-][0-9]+)?[flFL]?|"
-             r"0[xX][0-9a-fA-F]+[Ll]?", Number),
+            (r"0[xX][0-9a-fA-F]([_0-9a-fA-F]*[0-9a-fA-F])?[uU]?L?|"
+             r"0[bB][01]([_01]*[01])?[uU]?L?|"
+             r"\.[0-9]([_0-9]*[0-9])?([eE][+-]?[0-9]([_0-9]*[0-9])?)?[fF]?|"
+             r"(([0-9]([_0-9]*[0-9])?)(\.[0-9]([_0-9]*[0-9])?)?)(([eE][+-]?[0-9]([_0-9]*[0-9])?)[fF]?|[fF])|"
+             r"(0|[1-9]([_0-9]*[0-9])?)([uU]?L?)" , Number),
             # Identifiers
             (r'' + kt_id + r'((\?[^.])?)', Name) # additionally handle nullable types
         ],
